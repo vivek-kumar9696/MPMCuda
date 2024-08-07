9fc6a32acb631b263a7d76f18c446fbc27d53c1b
Vivek Kumar <31619951+vivek-kumar9696@users.noreply.github.com>
2024-08-06 20:36:42 -0400

    Add files via upload

---
+++ build/exampleList.js
@@ -0,0 +1,10 @@
+module.exports = [
+    './examples/helloTriangle',
+    './examples/simulation',
+    './examples/amongUs',
+    './examples/pikachu',
+    './examples/turkey',
+    './examples/twoHearts',
+    './examples/dogTurkeyHeart',
+    './examples/twoFluids',
+];
