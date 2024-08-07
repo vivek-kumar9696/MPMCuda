c5db9a7cd5d95b65ed539a89f04291e5d744f4a8
Vivek Kumar <31619951+vivek-kumar9696@users.noreply.github.com>
2024-08-06 20:36:22 -0400

    Add files via upload

---
+++ gen/exampleLoader.ts
@@ -0,0 +1,25 @@
+
+
+  export const helloTriangle = async () => (await import('../src/examples/helloTriangle'));
+  
+
+  export const simulation = async () => (await import('../src/examples/simulation'));
+  
+
+  export const amongUs = async () => (await import('../src/examples/amongUs'));
+  
+
+  export const pikachu = async () => (await import('../src/examples/pikachu'));
+  
+
+  export const turkey = async () => (await import('../src/examples/turkey'));
+  
+
+  export const twoHearts = async () => (await import('../src/examples/twoHearts'));
+  
+
+  export const dogTurkeyHeart = async () => (await import('../src/examples/dogTurkeyHeart'));
+  
+
+  export const twoFluids = async () => (await import('../src/examples/twoFluids'));
+  
